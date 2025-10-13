5b9287ffb11f883ff42e8df3dd06c19cc39fbb8a
borahazar <55186422+borahazar@users.noreply.github.com>
2025-10-13 10:42:36 +0300

    Initial Commit

---
+++ Bubble/BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleApp.swift
+//  Bubble
+//
+//  Created by BORA on 13.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
