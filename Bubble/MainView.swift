f8df9b0739c376054102bd11e64f1385525195cc
borahazar <55186422+borahazar@users.noreply.github.com>
2025-10-15 19:43:58 +0300

    checkpoint for settings view

--- Bubble/MainView.swift
+++ Bubble/MainView.swift
@@ -1,7 +1,18 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var isSettingsOpen = false
     var body: some View {
-        Text("Main View, To be continued...")
+        GeometryReader { geometry in
+            ZStack {
+                Color.blue.opacity(0.3)
+                    .ignoresSafeArea()
+                
+                SettingsView()
+                    .frame(width: geometry.size.width * 0.7)
+                    .offset(x: isSettingsOpen ? 0 : -geometry.size.width * 0.7)
+                    .animation(.easeInOut(duration: 0.3), value: isSettingsOpen)
+            }
+        }
     }
 }
