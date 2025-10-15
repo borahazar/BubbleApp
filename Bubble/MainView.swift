6f51d9fef6004af8f051c2ea4ad8225a5e4cbbf7
borahazar <55186422+borahazar@users.noreply.github.com>
2025-10-15 20:46:36 +0300

    settings view fixed

--- Bubble/MainView.swift
+++ Bubble/MainView.swift
@@ -4,14 +4,34 @@ struct MainView: View {
     @State private var isSettingsOpen = false
     var body: some View {
         GeometryReader { geometry in
-            ZStack {
+            ZStack(alignment: .leading){
                 Color.blue.opacity(0.3)
                     .ignoresSafeArea()
+                    .onTapGesture {
+                        if isSettingsOpen {
+                            isSettingsOpen = false
+                        }
+                    }
+                
+                VStack {
+                    HStack {
+                        Button {
+                            isSettingsOpen = true
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.title)
+                                .foregroundStyle(.white)
+                                .padding()
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                }
                 
                 SettingsView()
                     .frame(width: geometry.size.width * 0.7)
                     .offset(x: isSettingsOpen ? 0 : -geometry.size.width * 0.7)
-                    .animation(.easeInOut(duration: 0.3), value: isSettingsOpen)
+                    .animation(.easeInOut(duration: 0.4), value: isSettingsOpen)
             }
         }
     }
