c20c8dc5d763e56cb9fb5035572b0232c6981ace
borahazar <55186422+borahazar@users.noreply.github.com>
2025-10-25 23:04:11 +0300

    s w

--- Bubble/Settings/SettingsView.swift
+++ Bubble/Settings/SettingsView.swift
@@ -3,59 +3,58 @@ import SwiftUI
 struct SettingsView: View {
     @Bindable var viewModel: BubbleViewModel
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 30)
+        ZStack(alignment: .leading){
+            RoundedRectangle(cornerRadius: 2)
                 .fill(Color.purple.tertiary)
                 .ignoresSafeArea()
-            VStack(alignment: .leading, spacing: 40){
-                // Bubble Count Slider *****
-                GroupBox {
-                    Text("Bubble Count: \(Int(viewModel.bubbleCount))")
-                        .bold()
-                        .font(.title)
-                    Slider(value: $viewModel.bubbleCount, in: 1...100, step: 1)
-                        .tint(.black)
-                        
-                    HStack {
-                        Text("1")
-                            .font(.default)
-                            .foregroundStyle(.gray)
-                        Spacer()
-                        Text("100")
-                            .font(.default)
-                            .foregroundStyle(.gray)
-                    }.padding(.horizontal, 16)
-                }
-                // Bubble Color Picker
-                GroupBox {
-                    HStack {
-                        Text("Bubble Color: ")
+            ScrollView {
+                VStack(alignment: .leading, spacing: 40){
+                    // Bubble Count Slider *****
+                    GroupBox {
+                        Text("Bubble Count: \(Int(viewModel.bubbleCount))")
                             .bold()
                             .font(.title)
-                        Circle()
-                            .fill(viewModel.bubbleColor)
-                            .frame(width: 40, height: 40)
-                    }
-                    HStack {
-                        ForEach(viewModel.bubbleColorOptions, id: \.self) { color in
-                            Circle()
-                                .fill(color)
-                                .frame(width: 40, height: 40)
-                                .onTapGesture {
-                                    viewModel.bubbleColor = color
-                                }
+                        Slider(value: $viewModel.bubbleCount, in: 1...100, step: 1)
+                            .tint(.black)
+                        
+                        HStack {
+                            Text("1")
+                                .font(.default)
+                                .foregroundStyle(.gray)
+                            Text("100")
+                                .font(.default)
+                                .foregroundStyle(.gray)
                         }
                     }
-                }
-                // Bubble Velocity Slider
-                GroupBox {
-                    
+                    // Bubble Color Picker
+                    GroupBox {
+                        HStack {
+                            Text("Bubble Color: ")
+                                .bold()
+                                .font(.title)
+                            Circle()
+                                .fill(viewModel.bubbleColor)
+                                .frame(width: 20, height: 20)
+                        }
+                        HStack {
+                            ForEach(viewModel.bubbleColorOptions, id: \.self) { color in
+                                Circle()
+                                    .fill(color)
+                                    .frame(width: 20, height: 20)
+                                    .onTapGesture {
+                                        viewModel.bubbleColor = color
+                                    }
+                            }
+                        }
+                    }
+                    // Bubble Velocity Slider
+                    GroupBox {
+                        
+                    }
                 }
             }
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
-
-//#Preview {
-//    SettingsView(viewModel: BubbleViewModel())
-//}
